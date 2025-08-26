43d84ff6ea17e3961e8b916b41a917bfa4db2917
wangjingtong <2275491518@qq.com>
2025-08-26 12:13:24 +0800

    项目初始化

---
+++ src/main/java/itheima/SpringbootAdminClientApplication.java
@@ -0,0 +1,14 @@
+package itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminClientApplication.class, args);
+    }
+
+}
